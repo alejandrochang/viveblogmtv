17d98ffa5b35283c5bb75d088245a08ed25cc749
Alejandro Chang <alejandrochang10@gmail.com>
2024-01-15 17:44:04 -0800

    fix(vb): vive blog css nav

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 const Footer: React.FC = () => (
   <footer>
-    <p>© 2024 My Simple Blog</p>
+    <p>© 2024 ViveBlog</p>
   </footer>
 );
 
--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,11 +1,8 @@
-// src/components/Header.tsx
-
 import React from 'react';
 
 const Header: React.FC = () => (
   <header>
-    <h1>Vive Groups Blog</h1>
-    <p>HomeGroup Downtown Sunnyvale</p>
+    <p>Vive Blog: Downtown Sunnyvale</p>
     <nav>
       {/* Navigation links */}
     </nav>

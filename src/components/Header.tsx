c6bdda3019420683b2522954a16bfbc629e8d20a
Alejandro Chang <alejandrochang10@gmail.com>
2024-01-15 19:12:01 -0800

    feat(vb): adding logo and text

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,12 +1,13 @@
-import React from 'react';
+import React from "react";
 
-const Header: React.FC = () => (
-  <header>
-    <p>Vive Blog: Downtown Sunnyvale</p>
-    <nav>
-      {/* Navigation links */}
-    </nav>
-  </header>
-);
+const Header: React.FC = () => {
+  const url =
+    "https://images.squarespace-cdn.com/content/v1/5eb1edd795cde451fabc22c8/1590113908834-LHPUZYVBZJ0U1R1LSR1Y/VIVE_r3_05072020_white+%282%29.png?format=500w";
+  return (
+    <header>
+      <img src={url} alt="Vive Blog" /> <span>Sunnyvale Blog</span>
+    </header>
+  );
+};
 
 export default Header;

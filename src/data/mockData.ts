3e569db0036a1072f15e3d637f9c63a4fb1b5f27
Alejandro Chang <alejandrochang10@gmail.com>
2024-02-04 21:34:50 -0800

    feat(vb): add space text

--- src/data/mockData.ts
+++ src/data/mockData.ts
@@ -4,7 +4,7 @@ import WhatDidYouDo from "../assets/whatdidyoudo.jpg";
 import NoLongerLivingForMe from "../assets/nolongerlivingforme.jpg";
 
 export const mockData: BlogListProps = {
-  seriesScripture: ['Then Jesus said to his disciples, “Whoever wants to be my disciple must deny themselves and take up their cross and follow me. For whoever wants to save their life will lose it, but whoever loses their life for me will find it. What good will it be for someone to gain the whole world, yet forfeit their soul? Or what can anyone give in exchange for their soul?"'],
+  seriesScripture: ['Then Jesus said to his disciples, “Whoever wants to be my disciple must deny themselves and take up their cross and follow me. For whoever wants to save their life will lose it, but whoever loses their life for me will find it. What good will it be for someone to gain the whole world, yet forfeit their soul? Or what can anyone give in exchange for their soul?" '],
   posts: [
     {
       id: "no-longer-living-for-me",
